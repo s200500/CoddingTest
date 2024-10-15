a0a9b8b2620ca7da9eadfcbddf0b1198d0b95eb5
s200500 <139047751+s200500@users.noreply.github.com>
2024-10-15 22:18:04 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -3,22 +3,11 @@ class Solution {
         int answer = 0;
 
         char[] my_string_array = my_string.toCharArray();
-
         for (int i = 0; i < my_string_array.length; i++) {
-            if (my_string_array[i] == '1' ||
-                    my_string_array[i] == '2' ||
-                    my_string_array[i] == '3' ||
-                    my_string_array[i] == '4' ||
-                    my_string_array[i] == '5' ||
-                    my_string_array[i] == '6' ||
-                    my_string_array[i] == '7' ||
-                    my_string_array[i] == '8' ||
-                    my_string_array[i] == '9') {
-                System.out.println(my_string_array[i]);
+            if (my_string_array[i] > '0' && my_string_array[i] <= '9') {
                 answer += ((int) my_string_array[i]) - 48;
             }
         }
-
         return answer;
     }
 }
