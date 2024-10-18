445f566aa0a8c28d661b33ca204d6c91339ea160
s200500 <139047751+s200500@users.noreply.github.com>
2024-10-18 20:48:48 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,13 +1,6 @@
 class Solution {
     public int[] solution(int n) {
-        boolean numEvenOddCheck;
-
-        if (n % 2 == 0)
-            numEvenOddCheck = true;
-        else
-            numEvenOddCheck = false;
-
-        int[] answer = new int[numEvenOddCheck ? n / 2 : n / 2 + 1];
+        int[] answer = new int[(n % 2 == 0) ? n / 2 : n / 2 + 1];
 
         int count = 0;
 
