5badc50ebd8de3c78aa60497c26b10b7013da342
s200500 <139047751+s200500@users.noreply.github.com>
2024-10-24 19:11:50 +0900

    [level 0] Title: 0 떼기, Time: 13.91 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -2,13 +2,10 @@ class Solution {
     public String solution(String n_str) {
         String answer = "";
         char[] n_strArray = n_str.toCharArray();
-        boolean check = false;
 
         for (char c : n_strArray) {
-            if (c != '0' || check == true) {
+            if (c != '0' || answer != "")
                 answer += c;
-                check = true;
-            }
         }
         return answer;
     }
